8d071c0d82299bcf8063b2a406e9661148156137
luxkatana <taseen.bibi@gmail.com>
2023-10-18 21:43:03 +0200

    [beta] commiting using ToastNotifier

--- notifyserver/main.c
+++ notifyserver/main.c
@@ -5,8 +5,72 @@
 #include <strings.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+struct ThreadArgs
+{
+    struct sockaddr* casted_server;
+    int serverSocketDescriptor;
+    int *bouncing_descriptor;
+    int buffer_descriptor;
+    int new_one;
+};
+void *accept_thread(void *args)
+{
+    struct ThreadArgs *arguments = (struct ThreadArgs*) args;
+    struct sockaddr* server = arguments->casted_server;
+    int sizeofserver = sizeof(*server);
+    arguments->new_one = 0;
+    while(1)
+    {
+        int descripter = accept(arguments->serverSocketDescriptor, server, &sizeofserver);
+        printf("%d\n", descripter);
+        if(descripter < 0)
+        {
+            fprintf(stderr, "Could not Accept\n");
+        }
+    arguments->buffer_descriptor = descripter;
+    arguments->new_one = 1;
+        
+
+    }
+
+    
+}
+typedef struct {
+    int send_on_success; // file descriptor
+    int me;
+}BroadcastingStruct ;
+void* broadcast_thread(void* args)
+{
+    BroadcastingStruct *data = (BroadcastingStruct*) args;
+    for(;;)
+    {
+        char buffer[256];
+        int received_bytes = read((*data).me, buffer, sizeof(buffer));
+        if(received_bytes < 0)
+        {
+            fprintf(stderr, "Couldn't receive\n");
+            continue;
+        }
+        buffer[received_bytes] = '\0';
+        printf("received %s\n", buffer);
+        if(send((*data).send_on_success, buffer, strlen(buffer), 0) < 0)
+        {
+            fprintf(stderr, "Couldn't send data back\n");
+        }
+        else
+        {
+            printf("Sent %s\n", buffer);
+        }
+    }
+
+
+}
 int main()
 {
+
+    
     const int PORT = 9099;
     const int serverSocketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
     int other_descriptor_bounce = 0;
@@ -27,33 +91,46 @@ int main()
         fprintf(stderr, "Could not bind at %s:%d\n", bindTo, PORT);
         exit(1);
     }
-    if(listen(serverSocketDescriptor, 5) < 0)
-    {
-        fprintf(stderr, "Could not listen\n");
-        exit(1);
-    }
-    printf("Currently listening at %s:%d\n", bindTo, PORT);
+    listen(serverSocketDescriptor, 5);
+    printf("Currently listening at %s:%d\n", bindTo, PORT); 
+    pthread_t threading;
+    struct ThreadArgs ThreadingArguments;
+    ThreadingArguments.bouncing_descriptor = &other_descriptor_bounce;
+    ThreadingArguments.casted_server = server_as_sockaddr_casted;
+    ThreadingArguments.serverSocketDescriptor = serverSocketDescriptor;
+
+    pthread_create(&threading,NULL, accept_thread, (void*)&ThreadingArguments);
+    printf("Daemon started\n");
     for(;;)
     {
-         int serverSize = sizeof(server_address);
         char buffer[255];
-        int buffer_incoming_descriptor = accept(serverSocketDescriptor, server_as_sockaddr_casted, &serverSize);
-        fprintf(stdout, "Accepting..\n");
-        if (other_descriptor_bounce != 0 && buffer_incoming_descriptor > 0)
+        int serverSize = sizeof(server_address);
+        bzero(buffer, sizeof(buffer));
+        int buffer_incoming_descriptor = ThreadingArguments.buffer_descriptor;
+        // int buffer_incoming_descriptor = 0;
+        // printf("%d\n", buffer_incoming_descriptor);
+        if (ThreadingArguments.new_one == 1 && other_descriptor_bounce != 0 && buffer_incoming_descriptor > 0)
         {
             // other_descriptor_bounce = buffer_incoming_descriptor;
             // switch
-            if(read(buffer_incoming_descriptor, buffer, 255) < 0)
+            int bytes_recv = read(buffer_incoming_descriptor, buffer, 255);
+            if(bytes_recv < 0)
             {
                 fprintf(stderr, "Could not write inside\n");
                 break;
             }
             else
             {
-                buffer[strlen(buffer)] = '\0';
+                buffer[bytes_recv] = '\0';
                 printf("message: %s\n", buffer);
             }
-            if(send(other_descriptor_bounce, buffer, 255, 0) < 0)
+            if(send(buffer_incoming_descriptor, buffer, strlen(buffer), 0) < 0)
+            {
+                fprintf(stderr, "Could not echo back for verification\n");
+                break;
+            }
+            printf("Echo success %s\n", buffer);
+            if(send(other_descriptor_bounce, buffer, strlen(buffer), 0) < 0)
             {
                 fprintf(stderr, "Could not bounce back\n");
                 break;
@@ -61,18 +138,26 @@ int main()
             else
             {
                 fprintf(stdout, "Successfully sent!\n");
+                ThreadingArguments.new_one = 0;
+                BroadcastingStruct receive_this;
+                receive_this.me = buffer_incoming_descriptor;
+                receive_this.send_on_success = *(ThreadingArguments.bouncing_descriptor);
+                pthread_t thread_for_broadcasting;
+
+                pthread_create(&thread_for_broadcasting, NULL, broadcast_thread, (void*)&receive_this);
             }
         }
-        else if (other_descriptor_bounce == 0 && buffer_incoming_descriptor > 0)
+        else if (ThreadingArguments.new_one == 1 && other_descriptor_bounce == 0 && buffer_incoming_descriptor > 0)
         {
             other_descriptor_bounce = buffer_incoming_descriptor;
-            if(send(other_descriptor_bounce, "notifier=1", 12, 0) < 0)
+            if(send(other_descriptor_bounce, "notifier=1", 11, 0) < 0)
             {
-                fprintf(stderr, "Couldn't send bounce-back message");
+                fprintf(stderr, "Couldn't send bounce-back message\n");
             }
             else
             {
                 fprintf(stdout,"Elevated role\n");
+                ThreadingArguments.new_one = 0;
             }
         }
 
--- windowsclient/main.py
+++ windowsclient/main.py
@@ -1,5 +1,6 @@
 from socket import socket as ClientSocket
 from threading import Thread
+from wintoast import ToastNotifier
 SERVER_TO_LISTEN_IP: str =  '127.0.0.1'
 SERVER_TO_LISTEN_PORT: int = 9099
 
@@ -10,19 +11,40 @@ def yield_until_wifi() -> ClientSocket:
         try:
             buffer_socket.connect((SERVER_TO_LISTEN_IP, SERVER_TO_LISTEN_PORT))
         except: #noqa
-            print("ok got it")
+            ...
         else:
             return buffer_socket
 
-def listen(socket: ClientSocket):
+def listen(socket: ClientSocket, is_notifier: bool):
     while 1:
-        print('\n', socket.recv(270).decode())
+        content = socket.recv(257).decode()
+        if is_notifier:
+            toaster = ToastNotifier()
+            toaster.show_toast("From python", msg=content, duration=10, icon_path='')
+        print(content)
 if __name__ == '__main__':
     socket = yield_until_wifi()
     print("Connection estabilished")
-    while True:
-        if (input_ := input(">>").strip()):
+    socket.send("hello".encode())
+    first = socket.recv(200).decode()
+    print(first)
+    if first[:-1] == 'notifier=1':
+        Thread(target=listen, args=(socket, True), daemon=True).start()
+        print("Found server")
+        while True:
+            match input('>>'):
+                case 'exit':
+                    break
+    elif first == 'hello':
+        listener = Thread(target=listen, args=(socket, False), daemon=True)
+        listener.start()
+        print("Success")
+        while True:
+            if (input_ := input(">>").strip()):
+                continue
+            elif input_ == 'exit':
+                break
             socket.send(input_.encode())
-            print(f"Sent {input_}")
-
-    
+    else:
+        print("Server didn't respond back, closing")
+        socket.close()
